a7f912c76cc891ea8001a18f0368c25e3946ba15
hannu.eloranta <oh2lhe@gmail.com>
2022-05-15 06:31:22 +0300

    add: list view and string list model

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringListModel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QStringListModel *model;
 };
 #endif // MAINWINDOW_H
